d5c3aa8d56b4234a396d5ae19a8adb847502d028
Samira Mansur <samira.mansurc@gmail.com>
2024-05-14 23:30:35 -0300

    Atualizando

---
+++ Funcionario.py
@@ -0,0 +1,6 @@
+class Funcionario:
+    def __init__(self, ID, valor_servico, disponivel):
+        self.ID = ID
+        self.valor_servico = valor_servico
+        self.disponivel = disponivel
+        
